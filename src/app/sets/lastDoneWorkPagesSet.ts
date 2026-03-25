35d8f0708037b8e28810d73b1f5779c3ee6b516d
Keny2802 <oliva.vojta@gmail.com>
2026-03-25 20:35:42 +0100

    Přidání obrázků do AVIF fota ze sekce POSLEDNÍ DOKONČENÉ PRÁCE.

---
+++ src/app/sets/lastDoneWorkPagesSet.ts
@@ -0,0 +1,55 @@
+const lastDoneWorkPagesSet = [
+    {
+        heading: "Rekonstrukce lodžií v Habartově",
+        url: "/posledni-dokoncene-prace/rekonstrukce-lodzii-v-habartove",
+        src: [
+            "/posledni-dokoncene-prace/rekonstrukce-lodzii-v-habartove/rekonstrukce-lodzii-v-habartove-1.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-lodzii-v-habartove/rekonstrukce-lodzii-v-habartove-2.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-lodzii-v-habartove/rekonstrukce-lodzii-v-habartove-3.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-lodzii-v-habartove/rekonstrukce-lodzii-v-habartove-4.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-lodzii-v-habartove/rekonstrukce-lodzii-v-habartove-5.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-lodzii-v-habartove/rekonstrukce-lodzii-v-habartove-6.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-lodzii-v-habartove/rekonstrukce-lodzii-v-habartove-7.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-lodzii-v-habartove/rekonstrukce-lodzii-v-habartove-8.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-lodzii-v-habartove/rekonstrukce-lodzii-v-habartove-9.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-lodzii-v-habartove/rekonstrukce-lodzii-v-habartove-10.avif"
+        ],
+        alt: "Poslední dokončená práce rekonstrukce lodžií v Habartově - Jinex.cz | Jiří Nesměrák"
+    },
+    {
+        heading: "Rekonstrukce balkónů v Sokolově",
+        url: "/posledni-dokoncene-prace/rekonstrukce-balkonu-v-sokolove",
+        src: [
+            "/posledni-dokoncene-prace/rekonstrukce-balkonu-v-sokolove/rekonstrukce-balkonu-v-sokolove-1.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-balkonu-v-sokolove/rekonstrukce-balkonu-v-sokolove-2.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-balkonu-v-sokolove/rekonstrukce-balkonu-v-sokolove-3.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-balkonu-v-sokolove/rekonstrukce-balkonu-v-sokolove-4.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-balkonu-v-sokolove/rekonstrukce-balkonu-v-sokolove-5.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-balkonu-v-sokolove/rekonstrukce-balkonu-v-sokolove-6.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-balkonu-v-sokolove/rekonstrukce-balkonu-v-sokolove-7.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-balkonu-v-sokolove/rekonstrukce-balkonu-v-sokolove-8.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-balkonu-v-sokolove/rekonstrukce-balkonu-v-sokolove-9.avif",
+            "/posledni-dokoncene-prace/rekonstrukce-balkonu-v-sokolove/rekonstrukce-balkonu-v-sokolove-10.avif"
+        ],
+        alt: "Poslední dokončená práce rekonstrukce balkónů v Sokolově - Jinex.cz | Jiří Nesměrák"
+    },
+    {
+        heading: "Výroba a montáž nájezdu v Chodově",
+        url: "/posledni-dokoncene-prace/vyroba-a-montaz-najezdu-v-chodove",
+        src: [
+            "/posledni-dokoncene-prace/vyroba-a-montaz-najezdu-v-chodove/vyroba-a-montaz-najezdu-v-chodove-1.avif",
+            "/posledni-dokoncene-prace/vyroba-a-montaz-najezdu-v-chodove/vyroba-a-montaz-najezdu-v-chodove-2.avif",
+            "/posledni-dokoncene-prace/vyroba-a-montaz-najezdu-v-chodove/vyroba-a-montaz-najezdu-v-chodove-3.avif",
+            "/posledni-dokoncene-prace/vyroba-a-montaz-najezdu-v-chodove/vyroba-a-montaz-najezdu-v-chodove-4.avif",
+            "/posledni-dokoncene-prace/vyroba-a-montaz-najezdu-v-chodove/vyroba-a-montaz-najezdu-v-chodove-5.avif",
+            "/posledni-dokoncene-prace/vyroba-a-montaz-najezdu-v-chodove/vyroba-a-montaz-najezdu-v-chodove-6.avif",
+            "/posledni-dokoncene-prace/vyroba-a-montaz-najezdu-v-chodove/vyroba-a-montaz-najezdu-v-chodove-7.avif",
+            "/posledni-dokoncene-prace/vyroba-a-montaz-najezdu-v-chodove/vyroba-a-montaz-najezdu-v-chodove-8.avif",
+            "/posledni-dokoncene-prace/vyroba-a-montaz-najezdu-v-chodove/vyroba-a-montaz-najezdu-v-chodove-9.avif",
+            "/posledni-dokoncene-prace/vyroba-a-montaz-najezdu-v-chodove/vyroba-a-montaz-najezdu-v-chodove-10.avif"
+        ],
+        alt: "Poslední dokončená práce výroba a montáž nájezdu v městě Chodov, okres Karlovy Vary - Jinex.cz | Jiří Nesměrák"
+    },
+];
+
+export default lastDoneWorkPagesSet;
